6febe1f87d8d166d9f588cb091a2064b6c921d50
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-22 04:24:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ function deleteFromDriverByKey(driver, key) {
   delete newDriver.key;
   return newDriver;
 }
+
+function destructivelyDeleteFromDriverByKey(driver, key) { 
+  delete driver.key;
+} 
